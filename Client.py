cbd73f0f93c9c71ca687cd00db20886847669dd2
Salvader Ron Nathaniel <78250990+salvaderron@users.noreply.github.com>
2021-06-09 21:48:20 +0530

    Add files via upload

---
+++ Client.py
@@ -0,0 +1,34 @@
+import socket as s
+import cv2
+import pickle
+import struct
+
+client_s = s.socket(s.AF_INET,s.SOCK_STREAM)
+host_ip = '192.168.159.1' 
+port = 1234
+print("Socket Created Successfully")
+
+
+client_s.connect((host_ip,port))
+data = b""
+payload_size = struct.calcsize("Q")
+print("Socket Accepted")
+
+
+while True:
+    while len(data) < payload_size:
+        packet = client_s.recv(2160) 
+        if not packet: break
+        data+=packet
+    packed_msg_size = data[:payload_size]
+    data = data[payload_size:]
+    msg_size = struct.unpack("Q",packed_msg_size)[0]
+    
+    while len(data) < msg_size:
+        data += client_s.recv(2160)
+    frame_data = data[:msg_size]
+    data  = data[msg_size:]
+    frame = pickle.loads(frame_data)
+    cv2.imshow("RECEIVING VIDEO",frame)
+    if cv2.waitKey(10) == 13:
+        client_s.close()
